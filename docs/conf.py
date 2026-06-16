83909613ba2286988b80b6f1bec4ce89952b2881
tolgaozdogan263 <tolga.ozdogan44@gmail.com>
2026-06-16 09:38:17 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,2 +1,16 @@
-project = 'İlk Projem'
+```python
+import sphinx_rtd_theme
+
+# Buluta kurumsal temayı kullanmasını söylüyoruz
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# Şirket logosu ve başlık ayarları
+html_title = "Özdoğan Teknoloji - Geliştirici Portalı"
+html_logo = "_static/sirket_logosu.png" # _static klasöründeki logo resminin adı
+
+html_theme_options = {
+    'logo_only': True,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+}
